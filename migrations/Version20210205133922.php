c58ecba6ae5d82c0a713f7d1918d019d5f62c506
basmabsili <basmabsili12@gmail.com>
2021-02-05 18:36:13 +0100

    premier projet

---
+++ migrations/Version20210205133922.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210205133922 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contrat ADD km_depart INT NOT NULL, ADD km_retour INT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contrat DROP km_depart, DROP km_retour');
+    }
+}
